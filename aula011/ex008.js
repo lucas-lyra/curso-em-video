531f608faa9e3e2912aa9f02f842479e19687688
lucas-lyra <121949176+lucas-lyra@users.noreply.github.com>
2024-06-27 17:56:46 -0300

    novos estudos

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 30
+console.log(`a velociadede do seu carro ${vel}`);
+if (vel > 60 ){
+    console.log(`Voce ultrapasol a velocidade permitida. MULTADO!`);
+}
+console.log(`dirija sempre usando cinto de segurança!`);
---
+++ aula011/ex009.js
@@ -0,0 +1,7 @@
+var pais = `EUA`
+console.log(`videndo em ${pais}`);
+if (pais == `Brasil`){
+    console.log(` vc e Brasileiro`);
+}else{
+    console.log(`vc e estrangeiro `);
+}
