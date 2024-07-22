fec3cf4dc8ab6236ce66e8f964c6465f2c9cc119
lucas-lyra <121949176+lucas-lyra@users.noreply.github.com>
2024-07-22 11:17:13 -0300

    all

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,4 +1,5 @@
 let num = [5,8,4]
+num.sort()
 console.log(`noso vetor e o ${num}`);
 
 num[3] = 6
@@ -8,3 +9,12 @@ console.log(num);
 num.push(7)
 
 console.log(num);
+
+console.log(num.length);
+
+for(let pos = 0; pos< num.length ; pos ++){
+    console.log(num[pos]);
+}
+
+let pos = num.indexOf(8)
+console.log(pos);
